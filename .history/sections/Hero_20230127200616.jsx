d75bd1459bd1366274306463447a260dd946b710
duvalbarrett <duvalbarrett@gmail.com>
2023-01-27 20:10:00 -0500

    navbar header

---
+++ .history/sections/Hero_20230127200616.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+const Hero = () => (
+  <section className={`${styles.yPaddings} sm:p1-16 p1-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    />
+    <div className="flex justify-center items-center flex-col relative z-10">
+      
+    </div>
+  </section>
+);
+
+export default Hero;
---
+++ .history/sections/Hero_20230127200830.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+const Hero = () => (
+  <section className={`${styles.yPaddings} sm:p1-16 p1-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    />
+    <div className="flex justify-center items-center flex-col relative z-10">
+      <motion.h1
+        variants={textVariant(1.1)}
+className=""
+      >
+        Metaverse
+      </motion.h1>
+    </div>
+  </section>
+);
+
+export default Hero;
